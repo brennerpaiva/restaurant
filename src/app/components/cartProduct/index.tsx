62c445799b67a81ac098d772bebcc65acb454927
unknown <brennerpaiva@gmail.com>
2023-09-26 22:31:41 -0300

    feat: add cartProduct component

---
+++ src/app/components/cartProduct/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./cartProduct.modules.css";
+import CartUnits from "../cartUnits";
+
+export default function CartProduct() {
+  return (
+    <>
+      <div id="intensCarrinho" className="row mr-0 ml-0">
+        <div className="col-12 item-carrinho">
+          <div className="img-produto">
+            <img
+              src="/cardapio/burguers/burger-au-poivre-kit-4-pack.3ca0e39b02db753304cd185638dad518.jpg"
+              alt="produto"
+            />
+          </div>
+          <div className="dados-produto">
+            <p className="title-produto">
+              <strong>Nome do produto</strong>
+            </p>
+            <p className="price-produto">
+              <strong>R$ 149,99</strong>
+            </p>
+          </div>
+          <CartUnits />
+        </div>
+      </div>
+    </>
+  );
+}
